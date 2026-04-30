6e0027bb1cf47ecb554f5cda967880777963306f
Fares <votre@email.com>
2026-05-01 01:01:38 +0400

    remove broken folder

--- app/clients/new/page.tsx
+++
@@ -1,4 +0,0 @@
-﻿import Layout from '../../../components/Layout'
-import ClientForm from '../../../components/ClientForm'
-
-export default function NewClientPage() { return ( <Layout title='Nouveau client'><div className='bg-white rounded-3xl border border-gray-100 p-5'><ClientForm /></div></Layout> ) }
